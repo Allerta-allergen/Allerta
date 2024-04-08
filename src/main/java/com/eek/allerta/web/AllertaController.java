7476d08f615b1efafb1123746cf745ab55032c9d
git <git@example.com>
2024-04-08 19:14:30 +0300

    api/file for uploading a photo

--- src/main/java/com/eek/allerta/web/AllertaController.java
+++ src/main/java/com/eek/allerta/web/AllertaController.java
@@ -1,12 +1,24 @@
 package com.eek.allerta.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("api")
 public class AllertaController {
-    @RequestMapping("/hello")
+    @PostMapping("photo")
+    public String photo(@RequestParam("file") MultipartFile photo) throws IOException {
+        if(!photo.isEmpty()) {
+            byte[] bytes = photo.getBytes();
+            return Base64.getEncoder().encodeToString(bytes);
+        }
+        return null;
+    }
+
+    @GetMapping("hello")
     public String hello() {
         return "hello";
     }

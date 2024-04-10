09d0364b110baff7c1216eef26cd9f7b4f01ccf0
Anupam-1068 <107940692+Anupam-1068@users.noreply.github.com>
2024-04-10 08:49:54 +0300

    works now

--- src/Pages/ResultsPage.jsx
+++ src/Pages/ResultsPage.jsx
@@ -1,3 +1,127 @@
+// import React, { useState, useEffect } from 'react';
+// import { useLocation } from 'react-router-dom';
+// import { Box, Heading, Text, Divider, VStack, Card, Image } from '@chakra-ui/react';
+
+// const ResultsPage = () => {
+
+//   const location = useLocation();
+//   const imageData = location.state?.imageData; // Retrieve image data from location state
+//   console.log('Image data:', imageData); // Check the image data
+
+//   // Render loading state while waiting for data
+//   // if (!imageData) {
+//   //   return (
+//   //     <Box p={8}>
+//   //       <Text>Loading...</Text>
+//   //     </Box>
+//   //   );
+//   // }
+
+//   // Access data from the response
+//   // const data = imageData.data;
+//   // console.log('data',data);
+
+  
+
+//   // if (!data) {
+//   //   return (
+//   //     <Box p={8}>
+//   //       <Text>Loading...</Text>
+//   //     </Box>
+//   //   );
+//   // }
+
+//   // if (!data.is_ingredients_label) {
+//   //   return (
+//   //     <Box p={8}>
+//   //       <Text>Error: Ingredient label not found</Text>
+//   //     </Box>
+//   //   );
+//   // }
+
+//   const potentialAllergens = [];
+//   const otherIngredients = [];
+
+//   imageData.contains.forEach((ingredient, index) => {
+//     if (ingredient.user_allergen) {
+//       potentialAllergens.push(
+//         <Card key={index} p={4} flex="1" bg="gray.100">
+//           <Box>
+//             <Heading as="h2" size="lg" mb={4}>
+//               {ingredient.name}
+//             </Heading>
+//             <VStack align="start" spacing={4}>
+//               {ingredient.user_allergen && (
+//                 <Text color="red">You are allergic to {ingredient.name}</Text>
+//               )}
+//               {ingredient.description && <Text>{`Description: ${ingredient.description}`}</Text>}
+//             </VStack>
+//           </Box>
+//         </Card>
+//       );
+//     } else if (ingredient.potential_allergen || ingredient.user_allergen) {
+//       potentialAllergens.push(
+//         <Card key={index} p={4} flex="1" bg="gray.100">
+//           <Box>
+//             <Heading as="h2" size="lg" mb={4}>
+//               {ingredient.name}
+//             </Heading>
+//             <VStack align="start" spacing={4}>
+//               {ingredient.user_allergen && (
+//                 <Text color="red">You are allergic to {ingredient.name}</Text>
+//               )}
+//               {ingredient.potential_allergen && (
+//                 <Text color="red">You can be allergic to {ingredient.name}</Text>
+//               )}
+
+//               {ingredient.description && <Text>{`Description: ${ingredient.description}`}</Text>}
+//             </VStack>
+//           </Box>
+//         </Card>
+//       );
+//     } else {
+//       otherIngredients.push(
+//         <Card key={index} p={4} flex="1" bg="gray.100">
+//           <Box>
+//             <Heading as="h2" size="lg" mb={4}>
+//               {ingredient.name}
+//             </Heading>
+//             <VStack align="start" spacing={4}>
+//               {ingredient.description && <Text>{`Description: ${ingredient.description}`}</Text>}
+//             </VStack>
+//           </Box>
+//         </Card>
+//       );
+//     }
+//   });
+
+//   return (
+//     <Box p={8}>
+//       <Heading as="h1" size="xl">
+//         Results
+//       </Heading>
+//       <Divider my={6} />
+//       {image && <Image src={URL.createObjectURL(image)} alt="Uploaded" />}
+//       <VStack align="start" spacing={6}>
+//         {potentialAllergens}
+//         <Divider my={6} />
+//         <Heading as="h2" size="lg">
+//           Other Ingredients
+//         </Heading>
+//         {otherIngredients}
+//       </VStack>
+//       <Divider my={6} />
+//       <Heading as="h2" size="lg">
+//         Factory Processes:
+//       </Heading>
+//       <Text>{data.factory.processes[0].description}</Text>
+//     </Box>
+//   );
+// };
+
+// export default ResultsPage;
+
+
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Box, Heading, Text, Divider, VStack, Card, Image } from '@chakra-ui/react';
@@ -66,30 +190,37 @@ const ResultsPage = () => {
           }
         });
 
-  return (
-    <Box p={8}>
-      <Heading as="h1" size="xl">
-        Results
-      </Heading>
-      <Divider my={6} />
-      {imageUrl && <Image src={imageUrl} alt="Uploaded" />} {/* Use imageUrl */}
+// ... your existing code ...
+
+return (
+  <Box p={8}>
+    {/* ... your existing content ... */}
+    {imageData?.contains ? (
       <VStack align="start" spacing={6}>
         {potentialAllergens}
         <Divider my={6} />
         <Heading as="h2" size="lg">
           Other Ingredients
         </Heading>
-        {otherIngredients}
+        {imageData.contains.map((ingredient, index) => (
+          <Card key={index} p={4} flex="1" bg="gray.100">
+            <Box>
+              <Heading as="h2" size="lg" mb={4}>
+                {ingredient.name}
+              </Heading>
+              <VStack align="start" spacing={4}>
+                {ingredient.description && <Text>{`Description: ${ingredient.description}`}</Text>}
+              </VStack>
+            </Box>
+          </Card>
+        ))}
       </VStack>
-      <Divider my={6} />
-      <Heading as="h2" size="lg">
-        Factory Processes:
-      </Heading>
-      <Text>        
-        {imageData?.factory?.processes[0]?.description || 'No description available for factory processes'}
-      </Text> {/* Use optional chaining */}
-    </Box>
-  );
+    ) : (
+      <Text>No ingredients data available</Text>
+    )}
+  </Box>
+);
+
 };
 
 export default ResultsPage;
